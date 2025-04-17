4107264e20be0729c1c9960f6f29b858b5dfffef
AlvaroPerezPedreira <alvaro.perez0@outlook.com>
2025-04-17 18:00:35 +0200

    home update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/common/Navbar";
 import Carousel from "./pages/Carousel";
 import Home from "./pages/Home";
 import Card from "./pages/Card";
@@ -8,13 +6,16 @@ import Card from "./pages/Card";
 function App() {
   return (
     <>
-      <Navbar />
-      <div className="main-container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/3d-carousel" element={<Carousel />} />
-          <Route path="/3d-card" element={<Card />} />
-        </Routes>
+      <div className="app-container">
+        <div className="section-container">
+          <Home />
+        </div>
+        <div className="section-container">
+          <Carousel />
+        </div>
+        <div className="section-container">
+          <Card />
+        </div>
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,20 @@ import "./styles/home.css";
 export default function Home() {
   return (
     <div className="home-container">
-      <div className="home-description-border">
-        <div className="home-description-text">
-          This project is designed to practice and enhance CSS skills by
-          building interactive and visually appealing components. The primary
-          goal is to learn the core concepts of CSS, such as layout techniques,
-          animations, responsive design, and styling practices.
+      <div className="home-title-container">
+        <div className="home-title">CSS Project</div>
+        <div className="home-author">
+          I'm <span></span>
+        </div>
+      </div>
+      <div className="home-description-container">
+        <div className="home-description-border">
+          <div className="home-description-text">
+            This project is designed to practice and enhance CSS skills by
+            building interactive and visually appealing components. The primary
+            goal is to learn the core concepts of CSS, such as layout
+            techniques, animations, responsive design, and styling practices.
+          </div>
         </div>
       </div>
     </div>
